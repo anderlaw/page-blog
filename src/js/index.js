29e2f491eeb788f5c9699d4ccfe0eeab59dc273d
hezb <hezb@condata.cn>
2017-11-08 10:37:53 +0800

    quite

--- src/js/index.js
+++ src/js/index.js
@@ -15,16 +15,16 @@
     //父导航的hover bug修复
     let tempClearIden = null;
     let parentOfList = document.querySelector('nav li.haschild');
-    parentOfList.onmouseover = function(){
+    parentOfList.onmouseover = function () {
         //如果存在延迟调用直接清除
-        if(tempClearIden){
+        if (tempClearIden) {
             clearTimeout(tempClearIden)
         }
         this.classList.add('active')
     }
     parentOfList.onmouseout = function () {
         //先清除之前的延迟调用
-        if(tempClearIden){
+        if (tempClearIden) {
             clearTimeout(tempClearIden)
         }
         //重新设置延迟调用
@@ -50,15 +50,15 @@
     //搜索框事件
     let searBtn = document.querySelector('nav .list li.search > a');
     let ipt = document.querySelector('nav .list li.search > input');
-    searBtn.onclick = function(eve){
+    searBtn.onclick = function (eve) {
         //显示输入框
         document.querySelector('nav .list li.search > input').style.display = 'block';
         //隐藏按钮
         searBtn.style.display = 'none';
         return false;
-        
+
     }
-    ipt.onblur = function(){
+    ipt.onblur = function () {
         //隐藏输入框 
         this.style.display = 'none';
         //显示按钮
@@ -66,12 +66,21 @@
     }
     ///设置 图片的居中显示
     let picCenterEle = document.querySelector('#whychose .picture');
-    window.onresize = function(){
-        //获取视口宽度
-        let viewWidth = document.body.clientWidth;
-        //宽度为1170
-        let left = (viewWidth-1170)/2;
-        picCenterEle.style.left = left;
-        debugger
+    setEleCenter(picCenterEle,1170)
+    function setEleCenter(ele, width) {
+        //eley:原生element对象。width：整数类型
+        window.onresize = function(){
+            init()
+        }
+        //初始化
+        init();
+        function init(){
+            //获取视口宽度
+            let viewWidth = document.body.clientWidth;
+            //宽度为1170
+            let left = (viewWidth - width) / 2;
+            ele.style.left = left + 'px';
+        }
     }
+
 })()
